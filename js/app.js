150fe2388f35fb95cc157b4411a60bd5422a8a5a
Oluwatobiiiiii <odetokuntreasure6@gmail.com>
2023-01-30 14:30:36 +0100

    updated javascript

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ const hamburgerMenu = document.querySelector(".navbar-toogle");
 const menuIsActive = () => {
     hamburgerMenu.classList.toggle('active');
     document.querySelector(".navbar-menu").classList.toggle('active');
+    console.log("The navbar has been clicked");
 };
 
 hamburgerMenu.addEventListener('click', menuIsActive);
 
+// MANAGMENT
